7eab11ee94f609ed37f5b9da4e5b11e0647991c9
slow-groovin <136044792+slow-groovin@users.noreply.github.com>
2026-05-21 20:07:22 +0800

    "Clear project before refactor"

--- src/types/browser.ts
+++
@@ -1,3 +0,0 @@
-import { Browser } from "wxt/browser";
-
-export type PortListener = (message: unknown, port: Browser.runtime.Port) => void
--- src/types/message.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from "vue"
-
-export type UIMessage={
-  content: string,
-  at: number,
-  role: 'system'|'assistant'|'user',
-  hide?: boolean,
-}
-
